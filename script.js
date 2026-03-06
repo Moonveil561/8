8081b930b2e0d0a7d9223d299b7b6169daa8e13b
Moonveil561 <kiray.08@mail.ru>
2026-03-06 13:49:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -120,8 +120,8 @@ function startMagic(e) {
 
         const total = myPhotos.length;
         // УВЕЛИЧИЛИ РАДИУС ДЛЯ БОЛЬШЕГО РАССТОЯНИЯ
-        const radius = window.innerWidth > 480 ? 850 : 450; 
-
+      
+const radius = window.innerWidth > 480 ? 500 : 350;
         myPhotos.forEach((src, i) => {
             const item = document.createElement('div');
             item.className = 'carousel-item';
@@ -185,3 +185,4 @@ function closeModal() { document.getElementById('photoModal').style.display = "n
 
 initFlowers();
 animate();
+
